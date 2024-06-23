e35d9fa71b72bc18ca5ab07d76516d2974018ed4
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-06-23 18:53:24 +0600

    added slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../Components/Banner/Banner";
+import Slider from "../../Components/Slider/Slider";
 
 const Home = () => {
       return (
             <div>
                   <Banner />
+                  <Slider />
             </div>
       );
 };
